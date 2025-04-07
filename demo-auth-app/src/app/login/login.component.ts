9eab54dd9331c0d3f01947ac177840989df7856c
Kushpatel11 <gamdhakush@gmail.com>
2025-04-07 12:46:30 +0530

    ui_upgrade

---
+++ demo-auth-app/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule, NgClass, TitleCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [NgClass, TitleCasePipe, FormsModule, CommonModule],
+})
+export class LoginComponent {
+  email = signal('');
+  password = signal('');
+  showPassword = signal(false);
+  strength = signal<'weak' | 'medium' | 'strong' | null>(null);
+  isSubmitted = signal(false);
+
+  togglePassword(): void {
+    this.showPassword.set(!this.showPassword());
+  }
+
+  onPasswordInput(value: string) {
+    this.password.set(value);
+    const len = value.length;
+    this.strength.set(len < 6 ? 'weak' : len < 10 ? 'medium' : 'strong');
+  }
+  onSubmit(): void {
+    this.isSubmitted.set(true);
+  }
+}
